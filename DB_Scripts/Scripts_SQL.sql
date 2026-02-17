57a44967472ed42e4c3d26ee3a8209d604a82955
edusalees <eduardosousasales@gmail.com>
2026-02-17 00:37:42 -0300

    Procedures restantes para os Endpoits

--- DB_Scripts/Scripts_SQL.sql
+++ DB_Scripts/Scripts_SQL.sql
@@ -12,7 +12,7 @@ CREATE TABLE Veiculo
 
 use veiculos_db
 go
-CREATE PROCEDURE InsertVeiculo
+ALTER PROCEDURE InsertVeiculo
 @paramDescVeiculo VARCHAR(255),
 @paramMarcaVeiculo TINYINT,
 @paramModeloVeiculo VARCHAR(255),
@@ -25,12 +25,83 @@ VALUES(@paramDescVeiculo, @paramMarcaVeiculo, @paramModeloVeiculo, @paramOpciona
 SELECT SCOPE_IDENTITY()
 END
 
+----------------------------------=================================================
+
 use veiculos_db
 go
-CREATE PROCEDURE ExcluirRegistroVeiculo
+ALTER PROCEDURE ExcluirRegistroVeiculo
 @paramIdVeiculo BIGINT
 AS
 BEGIN
 UPDATE Veiculos
 SET RegistroAtivo = 0, DataInativacao = getdate()
+WHERE IdVeiculo = @paramIdVeiculo
 end
+-----------------------------------------------------------------
+use veiculos_db
+go
+ALTER PROCEDURE EditarRegistroVeiculo
+@paramIdVeiculo BIGINT,
+@paramDescVeiculo VARCHAR(255),
+@paramMarcaVeiculo TINYINT,
+@paramModeloVeiculo VARCHAR(255),
+@paramOpcionaisVeiculo VARCHAR(255),
+@paramValorVeiculo FLOAT
+AS
+BEGIN
+UPDATE 
+	Veiculos
+SET
+	DescVeiculo = CASE WHEN @paramDescVeiculo <> DescVeiculo THEN @paramDescVeiculo ELSE DescVeiculo END,
+	MarcaVeiculo = CASE WHEN @paramMarcaVeiculo <> MarcaVeiculo THEN @paramMarcaVeiculo ELSE MarcaVeiculo END,
+	ModeloVeiculo = CASE WHEN @paramModeloVeiculo <> ModeloVeiculo THEN @paramModeloVeiculo ELSE ModeloVeiculo END,
+	OpicionaisVeiculo = CASE WHEN @paramOpcionaisVeiculo <> OpicionaisVeiculo THEN @paramOpcionaisVeiculo ELSE OpicionaisVeiculo END,
+	ValorVeiculo = CASE WHEN @paramValorVeiculo <> ValorVeiculo THEN @paramValorVeiculo ELSE ValorVeiculo END
+WHERE 
+	IdVeiculo = @paramIdVeiculo
+END
+
+--===========================================================================
+
+USE veiculos_db
+GO
+ALTER PROCEDURE ListarTodosRegistrosVeiculos
+AS
+BEGIN
+SELECT
+	[IdVeiculo]
+   ,[DescVeiculo]
+   ,[MarcaVeiculo]
+   ,[ModeloVeiculo]
+   ,[OpicionaisVeiculo]
+   ,[ValorVeiculo]
+   ,[DataRegistro]
+FROM
+	Veiculos V
+WHERE
+	V.RegistroAtivo = 1 AND
+	V.DataInativacao IS NOT NUll
+END
+
+--==============================================================================]
+
+USE veiculos_db
+GO
+CREATE PROCEDURE ListarRegistrosVeiculoPorId
+@paramIdVeiculo BIGINT
+AS
+BEGIN
+SELECT
+	[IdVeiculo]
+   ,[DescVeiculo]
+   ,[MarcaVeiculo]
+   ,[ModeloVeiculo]
+   ,[OpicionaisVeiculo]
+   ,[ValorVeiculo]
+   ,[DataRegistro]
+FROM
+	Veiculos V
+WHERE 
+	V.IdVeiculo = @paramIdVeiculo AND
+	V.RegistroAtivo = 1
+END
